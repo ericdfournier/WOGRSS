2bb7757c62ef9549006030d11670bd39cd424a00
Eric Fournier <me@ericdfournier.com>
2014-07-10 14:36:04 -0700

    Major modifications to the huc zone extraction and grid mask generation functions. Also new spatial data files added for counties.

--- main.m
+++ main.m
@@ -2,3 +2,24 @@
 
 addpath(genpath('~/Repositories/WOGRSS'));
 cd ~/Repositories/WOGRSS
+
+%% Read in Spatial Data Files
+
+hucCodeShapePath = './data/huc10/huc10.shp';
+overlayShapePath = './data/counties/caCounties.shp';
+hucCodeShapeStruct = shaperead(hucCodeShapePath,'UseGeoCoords',true);
+overlayShapeStruct = shaperead(overlayShapePath,'UseGeoCoords',true);
+
+%% Select Study Site
+
+stateName = 'California';
+[hucID, hucIndex] = getHucCodeFnc(hucCodeShapeStruct,overlayShapeStruct,...
+    stateName);
+
+%% Generate Grid Mask
+
+gridDensity = 1116.99;
+[gridMask, spatialRef] = hucCode2GridMaskFnc(hucCodeShapeStruct,...
+    hucIndex,gridDensity);
+
+%% 
---
+++ src/hucCode2GridMaskFnc.m
@@ -0,0 +1,95 @@
+function [ gridMask, spatialRef ] = hucCode2GridMaskFnc( ...
+                                                hucCodeShapeStruct, ...
+                                                hucIndex, ...
+                                                gridDensity )
+
+% hucCode2GridMaskFnc.m Function to generate a binary gridMask from a 
+% scalar index value for a basin that has been selected from a 
+% hucCodeShapeStruct.
+%
+% DESCRIPTION:
+%
+%   Function which extracts and formats a binary grid mask layer for use in
+%   further analysis from an input basin code.
+% 
+%   Warning: minimal error checking is performed.
+%
+% SYNTAX:
+%
+%   [ gridMask, spatialRef ] =  hucCode2GridMaskFnc( ...
+%                                               hucCodeShapeStruct, ...
+%                                               hucIndex, ...
+%                                               gridDensity);
+%
+% INPUTS (REQUIRED): 
+%
+%   hucCodeShapeStruct = {g x 1} shapefile structure dataset in which each 
+%                       polygon corresponds to a delineated HUC basin
+%
+%   hucIndex =          [s] scalar vector index referencing the
+%                       hucCodeShapeStruct element corresponding to the 
+%                       basin that has been selected by the user for the 
+%                       production of the gridMask
+%
+%   gridDensity =       [p] scalar value indicating the desired number of 
+%                       grid cells per unit of latitude and longitude 
+%                       (a value of 10 indicates 10 cells per degree, for 
+%                       example)
+%
+% OUTPUTS:
+%
+%   gridMask =          [j x k] binary array with valid pathway grid cells 
+%                       labeled as ones and invalid pathway grid cells 
+%                       labeled as zero placeholders
+%
+%   spatialRef =        
+%
+% EXAMPLES:
+%   
+%   Example 1 =
+%
+% CREDITS:
+%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%                                                                      %%
+%%%                          Eric Daniel Fournier                        %%
+%%%                  Bren School of Environmental Science                %%
+%%%                 University of California Santa Barbara               %%
+%%%                                                                      %%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%% Parse Inputs
+
+P = inputParser;
+
+addRequired(P,'nargin',@(x)...
+    x == 3);
+addRequired(P,'nargout',@(x)...
+    x == 2);
+addRequired(P,'hucCodeShapeStruct',@(x)...
+    isstruct(x) &&...
+    ~isempty(x));
+addRequired(P,'hucIndex',@(x)...
+    isscalar(x) &&...
+    x > 0 && ...
+    ~isempty(x));
+addRequired(P,'gridDensity',@(x)...
+    isscalar(x) &&...
+    x > 0 && ...
+    ~isempty(x));
+
+parse(P,nargin,nargout,hucCodeShapeStruct,hucIndex,gridDensity);
+
+%% Extract Basin Geometry Data
+
+basinLon = hucCodeShapeStruct(hucIndex,1).Lon;
+basinLat = hucCodeShapeStruct(hucIndex,1).Lat;
+
+%% Generate Grid Mask Output
+
+[gridMask, spatialRef] = vec2mtx(basinLat,basinLon,gridDensity,'filled');
+gridMask(gridMask <= 1) = 1;
+gridMask(gridMask == 2) = 0;
+gridMask = gridMask(2:end-1,2:end-1);
+
+end
